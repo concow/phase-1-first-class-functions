336a91d4a0119f439748cf82a7075a207da7270c
Connor Cowan <ccowan003@comcast.net>
2021-07-20 14:28:41 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function nameFunction() {
+        console.log('Return a named function.');
+    }
+}
+
+function returnsAnAnonymousFunction () {
+    return () => console.log('Return an anonymous function.');
+}
